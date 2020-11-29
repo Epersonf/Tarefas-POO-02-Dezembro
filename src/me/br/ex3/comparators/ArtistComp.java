0b3e14beb796e59f350534d9896986c0dc839869
EPERSON <epersoncmxf@gmail.com>
2020-11-28 23:48:55 -0300

    Consertado problema em ordem alfabetica

--- src/me/br/ex3/comparators/ArtistComp.java
+++ src/me/br/ex3/comparators/ArtistComp.java
@@ -8,6 +8,6 @@ public class ArtistComp implements Comparator<Song> {
 
 	@Override
 	public int compare(Song o1, Song o2) {
-		return o1.getArtist().compareTo(o2.getArtist());
+		return -o1.getArtist().compareTo(o2.getArtist());
 	}
 }
--- src/me/br/ex3/comparators/NameComp.java
+++ src/me/br/ex3/comparators/NameComp.java
@@ -8,7 +8,7 @@ public class NameComp implements Comparator<Song> {
 
 	@Override
 	public int compare(Song o1, Song o2) {
-		return o1.getName().compareTo(o2.getName());
+		return -o1.getName().compareTo(o2.getName());
 	}
 	
 }
